d55a2836689e182bbfde4bfb66b5bdc04705a57c
wesleybertipaglia <wesleybertipaglia@gmail.com>
2025-05-06 20:02:36 -0300

    feat(database): add Docker Compose and SQL schema for MySQL database setup

---
+++ schema.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE IF NOT EXISTS taskdao;
+
+USE taskdao;
+
+CREATE TABLE IF NOT EXISTS tasks (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    description TEXT,
+    completed BOOLEAN DEFAULT FALSE
+);
