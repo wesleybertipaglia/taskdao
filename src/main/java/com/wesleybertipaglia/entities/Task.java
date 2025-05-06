c2121180d911f0931f905706e88c1341e4a43687
wesleybertipaglia <wesleybertipaglia@gmail.com>
2025-05-06 20:02:50 -0300

    feat(task): add Task class for task management with properties and methods

---
+++ src/main/java/com/wesleybertipaglia/entities/Task.java
@@ -0,0 +1,51 @@
+package com.wesleybertipaglia.entities;
+
+public class Task {
+
+    private int id;
+    private String title;
+    private String description;
+    private boolean completed;
+
+    public Task() {
+    }
+
+    public Task(String title, String description, boolean completed) {
+        this.title = title;
+        this.description = description;
+        this.completed = completed;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+}
